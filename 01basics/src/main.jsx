45f80e6a871ea6eab4965f570f42e4af42c71616
AkSharma2003 <ankit824012@gmail.com>
2025-12-31 13:39:22 +0530

    lecture4

--- 01basics/src/main.jsx
+++ 01basics/src/main.jsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+// const anotherElement = (
+//   <a href='https://google.com'>Visit Google</a>
+// )
+
+const anotherElement2 =React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'click Here to vist Google'
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  anotherElement2
 )
