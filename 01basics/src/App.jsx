e14ae80a363f6dace7517e15f3ca7e6c1e798f6d
AkSharma2003 <ankit824012@gmail.com>
2025-12-31 12:32:27 +0530

    first second and third lec.

---
+++ 01basics/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./chai"
+
+function App() {
+  return (
+    <>
+      <h1>Chai or Code</h1>
+      <Chai />
+    </>
+  )
+}
+
+export default App
---
+++ 01basics/src/chai.jsx
@@ -0,0 +1,8 @@
+
+function Chai(){
+    return(
+        <h1>Chai with Ankit</h1>
+    )
+}
+
+export default Chai
